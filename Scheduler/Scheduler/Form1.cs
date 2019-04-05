7164321f7d650d18a2df9188cbe26d9fee2e7c91
Joymar <lcbo@daum.net>
2019-04-05 15:05:33 +0900

    0
    Anchoring at github

---
+++ Scheduler/Scheduler/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scheduler
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			string version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			this.Text = string.Format("WorkerTimer 테스터 v{0}", version);
+			this.MinimumSize = this.Size;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
